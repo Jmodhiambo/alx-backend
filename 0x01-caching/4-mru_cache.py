f568a149a63d68a99140bb57bd8a7b2d6cf9f47e
jmodhiambo <jmodhiambo9346@gmail.com>
2025-05-15 03:36:41 +0300

    MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" MRUCache module """
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRUCache class that inherits from BaseCaching"""
+
+    def __init__(self):
+        """Initialize the cache"""
+        super().__init__()
+        self.usage_order = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.usage_order.remove(key)
+            self.usage_order.append(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                mru_key = self.usage_order[-1]
+                del self.cache_data[mru_key]
+                self.usage_order.pop()
+                print(f"DISCARD: {mru_key}")
+
+            self.cache_data[key] = item
+            self.usage_order.append(key)
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+
+        if key in self.usage_order:
+            self.usage_order.remove(key)
+        self.usage_order.append(key)
+        return self.cache_data[key]

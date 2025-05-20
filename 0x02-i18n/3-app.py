e62c2ef934de1de0a83d78d446c60ece2f900181
jmodhiambo <jmodhiambo9346@gmail.com>
2025-05-20 22:35:12 +0300

    Task 3. Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Flask app with Babel"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 
 class Config:
@@ -25,7 +25,7 @@ def get_locale():
 @app.route('/')
 def index():
     """Home page"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
